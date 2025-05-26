3143af8f2371268a028a2b74454f04a2e1dff200
CristhianBarbosaCarvalho <crizin9@gmail.com>
2025-05-26 07:49:40 -0300

    modificações da paginacontratada.xaml

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,38 @@
-﻿
+﻿using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suite Super Luxo",
+                ValorDiariaAdulto = 150.0,
+                ValorDiariaCrianca = 75.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suite Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suite Stand",
+                ValorDiariaAdulto = 90.0,
+                ValorDiariaCrianca = 45.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suite basic",
+                ValorDiariaAdulto = 70.0,
+                ValorDiariaCrianca = 35.0
+            }
+        };
+
         public App()
         {
             InitializeComponent();
